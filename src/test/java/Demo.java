7774bdc07820a20809b2cad41f5410a1fe0b1688
Adem Uras <adem.uras@ecozum.com>
2022-07-08 09:53:00 +0300

    ilk commit değişikliği

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Demo {
     public static void main(String[] args) {
-
+        //Commit için deneme
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver=new ChromeDriver();
         driver.manage().window().maximize();
